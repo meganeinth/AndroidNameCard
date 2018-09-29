b8847956ecfaf2d0899ca8a19a93ca9ea3850722
Ronnakit Boonpuncher <feadtodark@gmail.com>
2018-09-29 20:23:50 +0700

    fisrt init and  add function for working follow requirement

--- app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.navadon.androidnamecard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private View.OnClickListener onClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initOnClickListener();
+        findViewById(R.id.btn_next).setOnClickListener(onClickListener);
+
     }
+
+    // You don't have to bind any functions to "android:onClick" in layout XML file.
+    private void initOnClickListener(){
+        onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btn_next:
+                        Next();
+                        break;
+                }
+            }
+        };
+    }
+
+    private void Next(){
+        Intent intentNext = new Intent(this,InfoActivity.class);
+        startActivity(intentNext);
+    }
+
 }
